dc1fa1812d06fde4506cc25ac45df94d9346645e
Ervin <ervinchow@gmail.com>
2014-09-22 16:28:59 -0700

    "Reject" spec passed.

--- src/underbar.js
+++ src/underbar.js
@@ -96,6 +96,13 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+    _.each(collection, function(element){
+      if (!test(element)){
+        result.push(element);
+      }
+    });
+    return result;
   };
 
   // Produce a duplicate-free version of the array.
